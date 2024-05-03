6314fa50c21126297d73c477e232ecf72fab1df8
LiuYang <2391461132@qq.com>
2024-05-03 22:56:39 +0800

    feat: 添加redis相关配置和封装redis相关操作

---
+++ big-market-app/src/main/java/com/lyfx/config/RedisClientConfig.java
@@ -0,0 +1,75 @@
+package com.lyfx.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.BaseCodec;
+import org.redisson.client.protocol.Decoder;
+import org.redisson.client.protocol.Encoder;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+/**
+ * @author Yangfeixaio Liu
+ * @time 3/5/2024 下午8:27
+ * @description Redis 客户端使用Redisson
+ */
+@Configuration
+@EnableConfigurationProperties(RedisClientConfigProperties.class)
+public class RedisClientConfig {
+    @Bean("redissonClient")
+    public RedissonClient redissonClient(ConfigurableApplicationContext context, RedisClientConfigProperties properties) {
+        Config config = new Config();
+        
+        config.useSingleServer()
+                .setAddress("redis://"+properties.getHost()+":"+properties.getPort())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive());
+        config.setCodec(new RedisCodec());
+        
+        return Redisson.create(config);
+    }
+    
+    static class RedisCodec extends BaseCodec {
+        private final Encoder encoder = in -> {
+            ByteBuf out = ByteBufAllocator.DEFAULT.buffer();;
+            try {
+                ByteBufOutputStream os = new ByteBufOutputStream(out);
+                JSON.writeJSONString(os, in, SerializerFeature.WriteClassName);
+                return os.buffer();
+            } catch (IOException e) {
+                out.release();
+                throw e;
+            } catch (Exception e) {
+                out.release();
+                throw new IOException(e);
+            }
+        };
+        private final Decoder<Object> decoder = (buf, state) -> JSON.parseObject(new ByteBufInputStream(buf), Object.class);
+        @Override
+        public Decoder<Object> getValueDecoder() {
+            return decoder;
+        }
+        
+        @Override
+        public Encoder getValueEncoder() {
+            return encoder;
+        }
+    }
+}
--- big-market-domain/src/main/java/com/lyfx/domain/yyy/service/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.lyfx.domain.yyy.service;
