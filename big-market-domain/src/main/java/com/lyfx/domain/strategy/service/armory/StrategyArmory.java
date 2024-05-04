daff4d21d99b63aaaf3aa1bad025340f31336298
LiuYang <2391461132@qq.com>
2024-05-04 15:07:28 +0800

    fix: 修正概率值计算

--- big-market-domain/src/main/java/com/lyfx/domain/strategy/service/armory/StrategyArmory.java
+++ big-market-domain/src/main/java/com/lyfx/domain/strategy/service/armory/StrategyArmory.java
@@ -27,29 +27,24 @@ public class StrategyArmory implements IStrategyArmory {
                 .map(StrategyAwardEntity::getAwardRate)
                 .min(BigDecimal::compareTo)
                 .orElse(BigDecimal.ZERO);
-        // 3. 获取概率和的总和
-        BigDecimal totalAwardRate = strategyAwardEntities.stream()
-                .map(StrategyAwardEntity::getAwardRate)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        // 3. 循环计算找到概率值的范围
+        BigDecimal rateRange = BigDecimal.valueOf(convert(minAwardRate.doubleValue()));
         
-        // 4. 获得奖品随机值的总范围
-        BigDecimal rateRange = totalAwardRate.divide(minAwardRate, 0, RoundingMode.CEILING);
-        
-        // 5. 生成策略奖品查找表「这里指需要在list集合中，存放上对应的奖品占位即可，占位越多等于概率越高」
+        // 4. 生成策略奖品查找表「这里指需要在list集合中，存放上对应的奖品占位即可，占位越多等于概率越高」
         List<Integer> strategyAwardSearchTable = new ArrayList<>(rateRange.intValue());
         for (StrategyAwardEntity strategyAwardEntity : strategyAwardEntities) {
             Integer awardId = strategyAwardEntity.getAwardId();
             BigDecimal awardRate = strategyAwardEntity.getAwardRate();
-            // 计算出每种奖品需要填充的数量
-            for (int i = 0; i < rateRange.multiply(awardRate).setScale(0, RoundingMode.CEILING).intValue(); i++) {
+            // 计算出每种奖品需要填充的数量, 舍弃小数位
+            for (int i = 0; i < rateRange.multiply(awardRate).intValue(); i++) {
                 strategyAwardSearchTable.add(awardId);
             }
         }
         
-        // 6. 对存储的奖品进行乱序操作
+        // 5. 对存储的奖品进行乱序操作
         Collections.shuffle(strategyAwardSearchTable);
         
-        // 7. 生成Map集合，key的范围是随机值的范围，value是奖品ID
+        // 6. 生成Map集合，key的范围是随机值的范围，value是奖品ID
         Map<Integer, Integer> strategyAwardSearchMap = new LinkedHashMap<>();
         for (int i = 0; i < strategyAwardSearchTable.size(); ++i) {
             strategyAwardSearchMap.put(i, strategyAwardSearchTable.get(i));
@@ -58,11 +53,22 @@ public class StrategyArmory implements IStrategyArmory {
         // 8. 将Map集合存入redis中，方便之后查找
         // 这里使用strategyAwardSearchTable.size()而非rateRange
         // 是防止之后生成的随机数在table中对应为null
-        repository.storeStrategyAwardSearchRateTable(strategyId, strategyAwardSearchTable.size(), strategyAwardSearchMap);
+        repository.storeStrategyAwardSearchRateTable(strategyId, strategyAwardSearchMap.size(), strategyAwardSearchMap);
         
         return true;
     }
-
+    /**
+     * 转换计算，只根据小数位来计算。如【0.01返回100】、【0.009返回1000】、【0.0018返回10000】
+     */
+    private double convert(double min) {
+        double current = min;
+        double max = 1;
+        while (current < 1) {
+            current = current * 10;
+            max = max * 10;
+        }
+        return max;
+    }
     @Override
     public Integer getRandomAwardId(Long strategyId) {
         // 分布式部署下，不一定为当前应用做的策略装配。
