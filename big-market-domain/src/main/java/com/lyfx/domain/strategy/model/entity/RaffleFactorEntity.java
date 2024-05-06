4fe4b479c6bca65d4b563fe425fc6671d33423b9
liuyang <2391461132@qq.com>
2024-05-07 00:13:23 +0800

    feat: 添加抽奖相关实体

---
+++ big-market-domain/src/main/java/com/lyfx/domain/strategy/model/entity/RaffleFactorEntity.java
@@ -0,0 +1,22 @@
+package com.lyfx.domain.strategy.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Yangfeixiao Liu
+ * @time 2024-05-07 00:04
+ * @description 抽奖因子实体
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RaffleFactorEntity {
+    /** 用户ID */
+    private String userId;
+    /** 策略ID */
+    private Long strategyId;
+}
---
+++ big-market-domain/src/main/java/com/lyfx/domain/strategy/service/IRaffleStrategy.java
@@ -0,0 +1,10 @@
+package com.lyfx.domain.strategy.service;
+
+/**
+ * @author Yangfeixiao Liu
+ * @time 2024-05-06 11:58
+ * @description
+ */
+public interface IRaffleStrategy {
+    void performRaffle();
+}
